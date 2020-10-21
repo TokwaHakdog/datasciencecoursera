548e0470ae3ac8b7a44520918a40b95732974a56
AdrienG <gabinete.adrien@gmail.com>
2020-10-21 13:24:15 +0800

    R programming Week 1 notes: "subsetting"

---
+++ Subsetting.R
@@ -0,0 +1,9 @@
+#Notes on Subsetting
+## Basics
+  #operators to use for subsetting.
+#[ ]  <- always returns same class as original (subset vector get vector etc.)
+#can select more than one element
+
+#[[]] <- extract single element from list or data frame, class of the returned
+#object isnt necessarily a list or data frame 
+#$
